f243460aa58eda6fcd24cc67fd764db2d2a53f5a
renzzo1904 <lorenzo190400@gmail.com>
2024-04-15 13:47:58 -0600

    color dict for fixed color in dashboards

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -15,23 +15,20 @@ def get_percentage_change(my_list, return_str=True):
     Returns:
         str: The percentage change (rounded to 2 decimal places) or "nd%" if division by zero occurs.
     """
+    if my_list[-2] == 0: return "nd%"
     try:
         # Calculate the percentage change
         p = round((my_list[-1] - my_list[-2]) * 100 / my_list[-2], 2)
 
         # Return as a string with a percentage sign if requested
         if return_str:
-            return str(max(p, 0)) + "%"
+            return str(p)+ "%"
         else:
             return p
 
-    except ZeroDivisionError:
-        return "nd%"
-
     except IndexError:
         return "The elements in the list are not sufficient to perform the operation."
-
-      
+ 
 def generate_vector_components(speed):
     """
     Generates random components for latitude and longitude and scales them to match the desired speed.
@@ -50,7 +47,20 @@ def generate_vector_components(speed):
     vector_length = math.sqrt(lat_component**2 + long_component**2)
     
     # Scale the components to match the desired speed
-    scaled_lat_component = lat_component / vector_length * speed
-    scaled_long_component = long_component / vector_length * speed
+    scaled_lat_component = abs(lat_component / vector_length * speed)
+    scaled_long_component = abs(long_component / vector_length * speed)
     
     return scaled_lat_component, scaled_long_component
+
+def generate_color_dict(columns):
+    color_dict = {}
+    for column in columns:
+        # Generate random RGB values
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        # Convert RGB to hexadecimal
+        color = '#%02x%02x%02x' % (r, g, b)
+        # Assign color to column
+        color_dict[column] = color
+    return color_dict
